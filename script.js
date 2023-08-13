841e174c83163a1333677863c4a5bcbce33eaf67
luden127 <luden127@users.noreply.github.com>
2023-08-13 18:32:30 +0900

    a

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const positionElement = document.getElementById("position");
+var clickPosition = document.getElementById("clickposition");
 
 document.addEventListener("mousemove", (event) => {
     const x = event.clientX;
     const y = event.clientY;
     positionElement.textContent = `X: ${x}, Y: ${y}`;
+});
+
+button.addEventListener("click", function(event) {
+    // Get the mouse click coordinates relative to the viewport
+    var x = event.clientX;
+    var y = event.clientY;
+    
+    // Display the coordinates
+    alert("Mouse click coordinates: X=" + x + ", Y=" + y);
 });
